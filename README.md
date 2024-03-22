686aebccecea52839491933dc677ab0d7293d7d5
Brian Rayburn <bgrayburn@gmail.com>
2024-03-22 18:03:58 -0400

    add readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Gamepad2OSC
+
 Using OSC, I commonly found it would be useful to have some kind of hardware to trigger packets for testing or prototyping. To that end I built a webapp that uses the GameController and fetch api
 to send OSC messages in response to a game controller connected over bluetooth or usb.
 
 ## features
+
+- connect via websockets to a OSC server
+- send messages on button press
+- send messages on axis change including float values
 
 ## setup
 
@@ -10,8 +15,12 @@ to send OSC messages in response to a game controller connected over bluetooth o
 - run the app with `npm start`
 - open [http://localhost:3000](http://localhost:3000) to view it in the browser
 
-## todo
+## TODO
+
 - [ ] split osc and test panels
 - [ ] store settings in local storage
-- [ ] ability to save and load settings from file
-- [ ] ability to configure a button by pressing it instead of specifying button id
+- [ ] Ability to save/load mappings/servers
+- [ ] Flexbox for buttons in Gamepad section
+- [ ] Text for connection status is OSC section
+- [ ] Add ability to create mappings by touching button instead of entering its number
+  > > > > > > > Stashed changes
--- src/Components/OSCPanel.tsx
+++ src/Components/OSCPanel.tsx
@@ -1,82 +1,93 @@
-import React, {useState, useEffect, MutableRefObject} from 'react'
-import { toast } from 'react-hot-toast'
-import OSC from 'osc-js'
-import Panel from './Panel'
+import React, { useState, useEffect, MutableRefObject } from "react";
+import { toast } from "react-hot-toast";
+import OSC from "osc-js";
+import Panel from "./Panel";
 
 interface OSCPanelProps {
   oscRef: MutableRefObject<{}>;
 }
-const OSCPanel = ({oscRef}: OSCPanelProps) => {
-  const [osc, setOsc] = useState(new OSC())
-  const [host, setHost] = useState('localhost')
-  const [port, setPort] = useState(8081)
-  const [address, setAddress] = useState('/gamepad/test')
+const OSCPanel = ({ oscRef }: OSCPanelProps) => {
+  const [osc, setOsc] = useState(new OSC());
+  const [host, setHost] = useState("localhost");
+  const [port, setPort] = useState(8081);
+  const [address, setAddress] = useState("/gamepad/test");
 
   useEffect(() => {
-    oscRef.current = osc
-  }, [osc, oscRef])
- 
+    oscRef.current = osc;
+  }, [osc, oscRef]);
+
   const connectOSC = (host: string, port: number) => {
-    const newOsc = new OSC({ port, host })
-    newOsc.open({ port, host })
-    newOsc.sendOSCMessage = (message: string) => newOsc.send(message)
-    setOsc(newOsc)
-    return newOsc
-  }
+    const newOsc = new OSC({ port, host });
+    newOsc.open({ port, host });
+    newOsc.sendOSCMessage = (message: string) => newOsc.send(message);
+    setOsc(newOsc);
+    return newOsc;
+  };
   const disconnectOSC = () => {
-    osc.close()
-    osc.disconnect()
-  }
+    osc.close();
+    osc.disconnect();
+  };
   const sendOSCMessage = () => {
-    console.log('sending an OSC message')
-    const message = new OSC.Message(address)
-    toast(`Sending ${message.address} ${message.args} to ${osc.options.plugin.host}:${osc.options.plugin.port}`)
-    osc.send(message)
-  }
-  const connectionStatus = osc.options.plugin.socketStatus
+    console.log("sending an OSC message");
+    const message = new OSC.Message(address);
+    toast(
+      `Sending ${message.address} ${message.args} to ${osc.options.plugin.host}:${osc.options.plugin.port}`,
+    );
+    osc.send(message);
+  };
+  const connectionStatus = osc.options.plugin.socketStatus;
   const sections = [
     {
-      title: 'Host',
+      title: "Host",
       children: [
-        <input id='host' onChange={evt => setHost(evt.target.value)} value={host} />,
-        <span>{osc.options.plugin.host}</span>
-      ]
+        <input
+          id="host"
+          onChange={(evt) => setHost(evt.target.value)}
+          value={host}
+        />,
+        <span>{osc.options.plugin.host}</span>,
+      ],
     },
     {
-      title: 'Port',
+      title: "Port",
       children: [
-        <input id='port' type='number' onChange={evt => setPort(parseInt(evt.target.value) || 0)} value={port} />,
-        <span>{osc.options.plugin.port}</span>
-      ]
+        <input
+          id="port"
+          type="number"
+          onChange={(evt) => setPort(parseInt(evt.target.value) || 0)}
+          value={port}
+        />,
+        <span>{osc.options.plugin.port}</span>,
+      ],
     },
     {
-      title: 'Connection Status',
-      children: [
-        <span>{connectionStatus}</span>
-      ]
+      title: "Connection Status",
+      children: [<span>{connectionStatus}</span>],
     },
     {
-      title: 'Connection Actions',
+      title: "Connection Actions",
       children: [
-        <button onClick={() => connectOSC(host, port)} >Connect</button>,
-        <button onClick={() => disconnectOSC()} >Disconnect</button>
-      ]
+        <button onClick={() => connectOSC(host, port)}>Connect</button>,
+        <button onClick={() => disconnectOSC()}>Disconnect</button>,
+      ],
     },
     {
-      title: 'Test Address',
+      title: "Test Address",
       children: [
-        <input id='address' onChange={evt => setAddress(evt.target.value)} value={address} />,
-        <span>{osc.options.plugin.address}</span>
-      ]
+        <input
+          id="address"
+          onChange={(evt) => setAddress(evt.target.value)}
+          value={address}
+        />,
+        <span>{osc.options.plugin.address}</span>,
+      ],
     },
     {
-      title: 'Send Test Message',
-      children: [
-        <button onClick={sendOSCMessage}>send</button>
-      ]
-    }
-  ]
-  return <Panel title='OSC (test)' sections={sections} />
-}
+      title: "Send Test Message",
+      children: [<button onClick={sendOSCMessage}>send</button>],
+    },
+  ];
+  return <Panel title="OSC" sections={sections} />;
+};
 
-export default OSCPanel
+export default OSCPanel;
